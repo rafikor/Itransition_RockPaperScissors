f67716c0a0edefe10523f1bbb214d2c569f2b5d0
Arlou <orlovraf@gmail.com>
2023-03-13 12:25:11 +0300

    Rename variables for labels in CUI

--- RockPaperScissors/GameClass.cs
+++ RockPaperScissors/GameClass.cs
@@ -27,18 +27,16 @@ namespace RockPaperScissors
         private string[] options = { "Rock", "Paper", "Scissors" };
 
         private Label HMACLabel = new Label();
-
         private Label usernameLabel = new Label("Select your choise: ");
-
-        private ustring[] listOpts = new ustring[1];
-        private Button btnLogin = new Button("I made my choise");
+        private ustring[] listOptsLabels = new ustring[1];
+        private Button btnProcessResults = new Button("I made my choise");
         private Button btnNewGame = new Button("New game");
-        private Label playerMoveText = new Label();
-        private Label computerMoveText = new Label();
-        private Label winner = new Label();
-        private Label helpLinkToCheck = new Label("You can check HMAC here: https://dinochiesa.github.io/hmachash/index.html. Selection of the text at screen is performed by the mouse when shift is held");
-        private Label key = new Label();
-        private RadioGroup rad = new RadioGroup();
+        private Label playerMoveTextLabel = new Label();
+        private Label computerMoveTextLabel = new Label();
+        private Label winnerLabel = new Label();
+        private Label helpLinkToCheckLabel = new Label("You can check HMAC here: https://dinochiesa.github.io/hmachash/index.html. Selection of the text at screen is performed by the mouse when shift is held");
+        private Label keyLabel = new Label();
+        private RadioGroup radOptions = new RadioGroup();
 
         private int computerMove = -1;
         private int userMove = -1;
@@ -48,23 +46,23 @@ namespace RockPaperScissors
             computerMove = RandomNumberGenerator.GetInt32(0, options.Length);
             HMACLabel.Text = Convert.ToHexString(crypto.GenerateHMAC(options[computerMove]));
 
-            key.Text = "";
-            winner.Text = "";
-            computerMoveText.Text = "";
-            playerMoveText.Text = "";
-            btnLogin.IsDefault = true;
-            btnLogin.Enabled = true;
+            keyLabel.Text = "";
+            winnerLabel.Text = "";
+            computerMoveTextLabel.Text = "";
+            playerMoveTextLabel.Text = "";
+            btnProcessResults.IsDefault = true;
+            btnProcessResults.Enabled = true;
             btnNewGame.IsDefault = false;
-            rad.SetFocus();
+            radOptions.SetFocus();
         }
 
         private void processResults()
         {
             string winnerText = "";
-            if (rad.SelectedItem > -1 && rad.SelectedItem < options.Length)
+            if (radOptions.SelectedItem > -1 && radOptions.SelectedItem < options.Length)
             {
-                userMove = rad.SelectedItem;
-                playerMoveText.Text = "Your move is:" + options[userMove];
+                userMove = radOptions.SelectedItem;
+                playerMoveTextLabel.Text = "Your move is:" + options[userMove];
 
                 Winner winner = Rules.DetermineNumberOfWinner(computerMove, userMove, options.Length);
                 winnerText = winner switch
@@ -76,13 +74,13 @@ namespace RockPaperScissors
             }
             else
             {
-                if (rad.SelectedItem == options.Length)
+                if (radOptions.SelectedItem == options.Length)
                 {
                     Application.RequestStop();
                 }
                 else
                 {
-                    if (rad.SelectedItem == options.Length + 1)
+                    if (radOptions.SelectedItem == options.Length + 1)
                     {
                         var dialog = new Dialog("Help");
 
@@ -143,12 +141,12 @@ namespace RockPaperScissors
                     }
                 }
             }
-            key.Text = Convert.ToHexString(crypto.Key);
-            computerMoveText.Text = "Computer move is: " + options[computerMove];
+            keyLabel.Text = Convert.ToHexString(crypto.Key);
+            computerMoveTextLabel.Text = "Computer move is: " + options[computerMove];
 
-            winner.Text = winnerText;
-            btnLogin.IsDefault = false;
-            btnLogin.Enabled = false;
+            winnerLabel.Text = winnerText;
+            btnProcessResults.IsDefault = false;
+            btnProcessResults.Enabled = false;
             btnNewGame.IsDefault = true;
         }
 
@@ -161,7 +159,7 @@ public void Run(string[] _options)
             HMACLabel.Text = "";
             usernameLabel.Y = Pos.Bottom(HMACLabel);
 
-            listOpts = new ustring[options.Length + 2];
+            listOptsLabels = new ustring[options.Length + 2];
             for (int optIndex = 0; optIndex < options.Length; optIndex++)
             {
                 string numberStr = (optIndex + 1).ToString();
@@ -171,37 +169,37 @@ public void Run(string[] _options)
                 }
 
                 string optionText = numberStr + " - " + options[optIndex];
-                listOpts[optIndex] = optionText;
+                listOptsLabels[optIndex] = optionText;
             }
-            listOpts[options.Length] = "_0 - exit";
-            listOpts[options.Length+1] = "_h - help";
+            listOptsLabels[options.Length] = "_0 - exit";
+            listOptsLabels[options.Length+1] = "_h - help";
 
-            rad = new RadioGroup(listOpts)
+            radOptions = new RadioGroup(listOptsLabels)
             {
                 Y = Pos.Bottom(usernameLabel)
             };
 
-            btnLogin.Y = Pos.Bottom(rad) + 1;
-            btnLogin.IsDefault = true;
+            btnProcessResults.Y = Pos.Bottom(radOptions) + 1;
+            btnProcessResults.IsDefault = true;
 
-            btnNewGame.Y = Pos.Bottom(rad) + 1;
-            btnNewGame.X = Pos.Right(btnLogin) + 1;
+            btnNewGame.Y = Pos.Bottom(radOptions) + 1;
+            btnNewGame.X = Pos.Right(btnProcessResults) + 1;
 
-            playerMoveText.Y = Pos.Bottom(btnNewGame) + 1;
+            playerMoveTextLabel.Y = Pos.Bottom(btnNewGame) + 1;
 
-            computerMoveText.Y = Pos.Bottom(playerMoveText) + 1;
+            computerMoveTextLabel.Y = Pos.Bottom(playerMoveTextLabel) + 1;
 
-            winner.Y = Pos.Bottom(computerMoveText) + 1;
+            winnerLabel.Y = Pos.Bottom(computerMoveTextLabel) + 1;
 
-            key.Y = Pos.Bottom(winner) + 1;
+            keyLabel.Y = Pos.Bottom(winnerLabel) + 1;
 
-            helpLinkToCheck.Y = Pos.Bottom(key) + 1;
+            helpLinkToCheckLabel.Y = Pos.Bottom(keyLabel) + 1;
 
-            btnLogin.Clicked += processResults;
+            btnProcessResults.Clicked += processResults;
 
             btnNewGame.Clicked += InitNewGame;
 
-            Application.Top.Add(HMACLabel, usernameLabel, rad, btnLogin, btnNewGame, playerMoveText, computerMoveText, winner, key, helpLinkToCheck);
+            Application.Top.Add(HMACLabel, usernameLabel, radOptions, btnProcessResults, btnNewGame, playerMoveTextLabel, computerMoveTextLabel, winnerLabel, keyLabel, helpLinkToCheckLabel);
             InitNewGame();
             Application.Run();
             Application.Shutdown();
