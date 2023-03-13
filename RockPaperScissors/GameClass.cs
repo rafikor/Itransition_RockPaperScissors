a42427778b3746f7be6bace2eca720005e40e13a
Arlou <orlovraf@gmail.com>
2023-03-13 12:33:56 +0300

    Move generation of help Win/Lose table to separate class

--- RockPaperScissors/GameClass.cs
+++ RockPaperScissors/GameClass.cs
@@ -87,32 +87,7 @@ namespace RockPaperScissors
                         var helpDialogLabel = new Label();
                         helpDialogLabel.Text = "Values in cells give result for the player with moves specified in head of table";
 
-                        DataTable dataTable = new DataTable("Help");
-
-                        dataTable.Columns.Add(new DataColumn("Moves", typeof(string)));
-
-                        for (int i = 0;i < options.Length;i++)
-                        {
-                            dataTable.Columns.Add(new DataColumn(options[i], typeof(string)));
-                        }
-
-                        for (int i = 0; i < options.Length; i++)//to the down
-                        {
-                            object[] row = new object[options.Length+1];
-                            row[0] = options[i];
-                            for (int j = 0; j < options.Length; j++)//to the right
-                            {
-                                Winner winner=Rules.DetermineNumberOfWinner(i, j, options.Length);
-                                row[j + 1] = winner switch
-                                {
-                                    Winner.FirstPlayerWin => "Lose",
-                                    Winner.SecondPlayerWin => "Win",
-                                    _ => "Draw"
-                                };
-                            }
-                            dataTable.Rows.Add(row);
-                        }
-
+                        DataTable dataTable = TableWinLoseGenerator.GenerateTableWinLose(options);
 
                         TableView table = new TableView();
                         table = new TableView()
---
+++ RockPaperScissors/TableWinLoseGenerator.cs
@@ -0,0 +1,43 @@
+﻿using NStack;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class TableWinLoseGenerator
+    {
+        public static DataTable GenerateTableWinLose(string[] options)
+        {
+            DataTable dataTable = new DataTable("Help");
+
+            dataTable.Columns.Add(new DataColumn("Moves", typeof(string)));
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                dataTable.Columns.Add(new DataColumn(options[i], typeof(string)));
+            }
+
+            for (int i = 0; i < options.Length; i++)//to the down
+            {
+                object[] row = new object[options.Length + 1];
+                row[0] = options[i];
+                for (int j = 0; j < options.Length; j++)//to the right
+                {
+                    Winner winner = Rules.DetermineNumberOfWinner(i, j, options.Length);
+                    row[j + 1] = winner switch
+                    {
+                        Winner.FirstPlayerWin => "Lose",
+                        Winner.SecondPlayerWin => "Win",
+                        _ => "Draw"
+                    };
+                }
+                dataTable.Rows.Add(row);
+            }
+            return dataTable;
+        }
+    }
+}
